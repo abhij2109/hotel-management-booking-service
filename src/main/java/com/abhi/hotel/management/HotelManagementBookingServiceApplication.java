3ed680b0ac63ff87bb0834e32abec5b5c0f4e6c2
abhij2109 <abhij2116@gmail.com>
2024-12-11 19:33:43 +0530

    Project Creation and First commit.

---
+++ src/main/java/com/abhi/hotel/management/HotelManagementBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.abhi.hotel.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementBookingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhi/hotel/management/HotelManagementBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.hotel.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
