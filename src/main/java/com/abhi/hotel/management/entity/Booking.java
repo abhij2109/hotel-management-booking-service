27ec469ac7ab9c3fadc918a4f7820adde6d58672
abhij2109 <abhij2116@gmail.com>
2024-12-12 00:48:41 +0530

    Created the Booking fields.

--- src/main/java/com/abhi/hotel/management/entity/Booking.java
+++ src/main/java/com/abhi/hotel/management/entity/Booking.java
@@ -1,1 +1,36 @@
+package com.abhi.hotel.management.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Check In Date is required.")
+    private LocalDate checkInDate;
+
+    @Future(message = "Check Out Date is required.")
+    private LocalDate checkOutDate;
+
+    private Integer numOfAdults;
+    private Integer numOfChild;
+    private Integer totalNumOfGuests;
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User roomUser;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+}
