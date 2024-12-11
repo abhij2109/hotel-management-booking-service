d7a93064f31a5defd0c47eb0e4e6cdabca4fbc42
abhij2109 <abhij2116@gmail.com>
2024-12-11 20:05:07 +0530

    Created the user fields.

--- src/main/java/com/abhi/hotel/management/entity/User.java
+++ src/main/java/com/abhi/hotel/management/entity/User.java
@@ -1,4 +1,32 @@
 package com.abhi.hotel.management.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Name is required.")
+    private String name;
+
+    @NotNull(message = "Email is required.")
+    @Column(unique = true)
+    private String email;
+
+    @NotNull(message = "Email is required.")
+    private String phoneNumber;
+
+    private String role;
+
+    @OneToMany
+    private List<Booking> bookings = new ArrayList<>();
 }
