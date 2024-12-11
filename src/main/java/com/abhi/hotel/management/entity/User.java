d7215ee3a8a1bc7b2b92830ade150234520e602d
abhij2109 <abhij2116@gmail.com>
2024-12-12 01:46:25 +0530

    Completed the User Entity fields.

--- src/main/java/com/abhi/hotel/management/entity/User.java
+++ src/main/java/com/abhi/hotel/management/entity/User.java
@@ -1,15 +1,20 @@
 package com.abhi.hotel.management.entity;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Data
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,11 +27,44 @@ public class User {
     @Column(unique = true)
     private String email;
 
-    @NotNull(message = "Email is required.")
+    @NotNull(message = "Phone Number is required.")
     private String phoneNumber;
+
+    @NotNull(message = "Password is required.")
+    private String password;
 
     private String role;
 
-    @OneToMany
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Booking> bookings = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
