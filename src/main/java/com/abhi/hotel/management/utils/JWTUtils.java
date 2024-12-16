695559518c47d6eaa7efa247afb4dea619caa496
abhij2109 <abhij2116@gmail.com>
2024-12-16 23:16:49 +0530

    JWT utils for token generation and validation.

---
+++ src/main/java/com/abhi/hotel/management/utils/JWTUtils.java
@@ -0,0 +1,54 @@
+package com.abhi.hotel.management.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JWTUtils {
+
+    private static final long EXPIRATION_TIME = 1000*60*24*7;
+    private SecretKey key = null;
+
+    public JWTUtils(){
+        String secretString = "87379792473979273749R5S1T5D87379792473979273749R5S1T5D87379792473979273749R5S1T5D87379792473979273749R5S1T5D";
+        byte[] keyBytes = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
+        this.key = new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUserName(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsTFunction) {
+        return claimsTFunction.apply(Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload());
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        String userName = extractUserName(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+
+}
