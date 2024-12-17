7ec203fe440be1a93322a8314dd5aac8f44ed09c
abhij2109 <abhij2116@gmail.com>
2024-12-18 01:38:11 +0530

    WIP : Configuring Security

---
+++ src/main/java/com/abhi/hotel/management/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.abhi.hotel.management.security;
+
+import com.abhi.hotel.management.service.impl.CustomUserServiceImplementation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomUserServiceImplementation customUserServiceImplementation;
+    private final JWTAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth->auth.
+                        requestMatchers("/api/**").authenticated().anyRequest().permitAll());
+
+
+        return null;
+    }
+}
